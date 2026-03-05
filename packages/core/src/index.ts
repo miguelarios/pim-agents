84acfb697f9a539085a45be8a0edc6fdec605105
Miguel <24581058+miguelarios@users.noreply.github.com>
2026-03-04 18:32:17 -0600

    feat(core): export public API from index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,22 @@
-export {};
+export {
+  type CardDavConfig,
+  loadCardDavConfig,
+} from "./config.js";
+
+export {
+  AuthenticationError,
+  ConfigurationError,
+  ConnectionError,
+  ContactError,
+  ErrorCode,
+  PimError,
+  ValidationError,
+  isRetryableError,
+  toPimError,
+} from "./errors.js";
+
+export {
+  type Contact,
+  buildVCard,
+  parseVCard,
+} from "./vcard.js";
